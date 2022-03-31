70f5b9ddb6449c37de78d7a023c82c6cf7a316fd
Tyler Parker <tparker@computemachines.com>
2022-03-31 15:34:33 -0400

    Add circle shape. Faster compiles on linux.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,45 +1,84 @@
 mod line_mesh;
+mod shapes;
 
-use bevy::sprite::Mesh2dHandle;
-use bevy::{prelude::*, render::mesh::Indices};
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::render::render_resource::PrimitiveTopology;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
+use bevy::{prelude::*, render::mesh::Indices};
 
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 use quad_edge::delaunay_voronoi::DelaunayMesh;
 
-
 pub fn explore_mesh(mesh: DelaunayMesh) {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(line_mesh::LineMeshPlugin)
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        // .add_plugin(line_mesh::LineMeshPlugin)
+        // .add_plugin(FrameTimeDiagnosticsPlugin::default())
         // .add_plugin(EguiPlugin)
         // Systems that create Egui widgets should be run during the `CoreStage::Update` stage,
         // or after the `EguiSystem::BeginFrame` system (which belongs to the `CoreStage::PreUpdate` stage).
         // .add_system(ui_example)
         // .insert_resource(Msaa { samples: 1 })
         .add_startup_system(setup_system)
-        .add_startup_system(add_lines)
+        // .add_startup_system(add_lines)
+        // .add_system(animate_view)
+        .add_system(animate_line)
+        // .add_system(debug)
         .run();
 }
 
-fn setup_system(mut commands: Commands) {
+fn animate_view(mut transform: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
+    // transform.single_mut().translation.y = time.seconds_since_startup() as f32 * 10.;
+}
+
+#[derive(Component)]
+struct AnimatedLine;
+
+fn animate_line(mut query: Query<(Entity, &mut Transform), With<AnimatedLine>>, time: Res<Time>) {
+    if let (entity, mut transform) = query.single_mut() {
+        transform.translation.x = time.seconds_since_startup() as f32 * 10.0;
+        // info!("Animate line called with: {}", entity.id());
+    } else {
+        error!("Animate line system called with bad data");
+    }
+}
+
+fn setup_system(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::ALICE_BLUE,
+                custom_size: Some(Vec2::new(1., 100.)),
+                ..Default::default()
+            },
+            transform: Transform::from_rotation(Quat::from_rotation_z(3.14 / 80.)),
+            ..Default::default()
+        })
+        .insert(AnimatedLine);
+    commands.spawn_bundle(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shapes::Circle::new(100., 16))).into(),
+        material: materials.add(ColorMaterial::from(Color::GREEN)),
+        ..Default::default()
+    });
+}
+
+fn debug(query: Query<(Entity, &Mesh2dHandle)>, meshes: Res<Assets<Mesh>>) {
+    for (entity, mesh) in query.iter() {
+        info!("Entity({}).mesh = {:?}", entity.id(), meshes.get(&mesh.0));
+    }
 }
 
 fn add_lines(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     let mut lines = Mesh::new(PrimitiveTopology::LineList);
-    let mut v_pos = vec![
-        [0., 0., 0.],
-        [100., 100., 0.],
-    ];
+    let mut v_pos = vec![[0., 0., 0.], [100., 100., 0.]];
     lines.set_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
 
-    let mut v_color = vec![
-        [1., 0., 0., 1.],
-        [0., 1., 0., 1.],
-    ];
+    let mut v_color = vec![[1., 0., 0., 1.], [0., 1., 0., 1.]];
     lines.set_attribute(Mesh::ATTRIBUTE_COLOR, v_color);
 
     let indices: Vec<u32> = vec![0, 1];
@@ -48,10 +87,10 @@ fn add_lines(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     commands.spawn_bundle((
         line_mesh::LineMesh::default(),
         Mesh2dHandle(meshes.add(lines)),
-
         Transform::default(),
         GlobalTransform::default(),
         Visibility::default(),
         ComputedVisibility::default(),
+        AnimatedLine,
     ));
 }
---
+++ src/gui/shapes.rs
@@ -0,0 +1,54 @@
+use bevy::{prelude::*, render::{render_resource::PrimitiveTopology, mesh::Indices}};
+
+#[derive(Debug, Clone)]
+pub struct Circle {
+    radius: f32,
+    num_segments: usize,
+}
+
+impl Circle {
+    pub fn new(radius: f32, num_segments: usize) -> Self {
+        Self {
+            radius,
+            num_segments,
+        }
+    }
+}
+
+impl From<Circle> for Mesh {
+    fn from(circle: Circle) -> Self {
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let mut positions = Vec::with_capacity(circle.num_segments+1);
+        let normals = (0..circle.num_segments+1).into_iter().map(|_| [0., 0., 1.,]).collect::<Vec<_>>();
+        let mut uvs = Vec::with_capacity(circle.num_segments+1);
+
+        positions.push([0., 0., 0.]);
+        uvs.push([0.5, 0.5]);
+        for i in 0..circle.num_segments {
+            positions.push([
+                circle.radius * f32::cos(i as f32 * std::f32::consts::TAU / circle.num_segments as f32),
+                circle.radius * f32::sin(i as f32 * std::f32::consts::TAU / circle.num_segments as f32),
+                0.,
+            ]);
+            uvs.push([
+                0.5 * f32::cos(i as f32 * std::f32::consts::TAU / circle.num_segments as f32) + 0.5,
+                0.5 * f32::sin(i as f32 * std::f32::consts::TAU / circle.num_segments as f32) + 0.5,
+            ])
+        }
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+
+
+        let mut indices = Vec::with_capacity(circle.num_segments);
+        for i in 1..circle.num_segments as u32 {
+            indices.extend_from_slice(&[
+                0, i, i+1,
+            ]);
+        }
+        indices.extend_from_slice(&[0, circle.num_segments as u32, 1]);
+        mesh.set_indices(Some(Indices::U32(indices)));
+        
+        mesh
+    }
+}
